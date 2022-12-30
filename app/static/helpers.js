d7bc94d7a5c2aced567b82a5c824a0b65a712308
Sean Rawson <seancc8@gmail.com>
2022-12-30 09:32:03 -0500

    Refactor helper functions
    Update html template to use refactored helpers

--- app/static/helpers.js
+++ app/static/helpers.js
@@ -1,4 +1,4 @@
-const dirs = ["Outbound", "Inbound"]
+const dirs = ['outbound', 'inbound']
 
 function addEventListeners() {
     let routeElement = getRouteElement();
@@ -11,38 +11,69 @@ function addEventListeners() {
 }
 
 function selectRoute(route) {
-    if (route != "")
-        window.location.assign(window.origin + `/routes/${route}`);
+    if (route != '')
+        window.location.assign(buildPath(route));
 }
 
 function selectDir(dir) {
-    let pathElems = window.location.pathname.split("/");
-    if (dir != "") {
-        if (dirs.includes(pathElems[pathElems.length - 1])) {
-            let path = pathElems.slice(1, pathElems.length - 1).join("/")
-            window.location.assign(window.origin + `/${path}/${dir}`);
-        }
-        else
-            window.location.assign(window.location + `/${dir}`);
+    let pathElems = window.location.pathname.split('/');
+    if (dir != '') {
+        let routeIdx = pathElems.indexOf('routes') + 1;
+        window.location.assign(buildPath(pathElems[routeIdx], dir));
     }
 }
 
 function populateSelections() {
-    let routeElement = getRouteElement();
-    let pathElems = window.location.pathname.split("/");
-    if (pathElems.length == 3) {
-        routeElement.value = pathElems[pathElems.length - 1];
-    } else if (pathElems.length == 4) {
-        routeElement.value = pathElems[pathElems.length - 2];
-        let dirElement = getDirElement();
-        dirElement.value = pathElems[pathElems.length - 1];
+    let pathElems = window.location.pathname.split('/');
+
+    let startIdx = pathElems.indexOf('routes');
+    let fieldCandidates = [null, null];
+
+    if (startIdx++ != -1) {
+        let fieldIdx = 0;
+        while (fieldIdx < fieldCandidates.length && startIdx < pathElems.length) {
+            fieldCandidates[fieldIdx] = pathElems[startIdx];
+            fieldIdx++;
+            startIdx++;
+        }
+    }
+
+    if (fieldCandidates[0] != null) {
+        getRouteElement().value = fieldCandidates[0];
+        if (fieldCandidates[1] != null)
+            getDirElement().value = fieldCandidates[1];
     }
 }
 
+function createStopLinks() {
+    let route = getRouteElement().value;
+    let direction = getDirElement().value;
+    let stopElems = document.getElementsByClassName('stop');
+    for (let stop of stopElems) {
+        let pathElems = [route, direction, stop.id];
+        let path = pathElems.join('/');
+        stop.setAttribute('href', window.origin + `/routes/${path}`);
+    }
+}
+   
+
 function getRouteElement() {
-    return document.getElementById("route_list");
+    return document.getElementById('route_list');
 }
 
 function getDirElement() {
-    return document.getElementById("dir_list");
+    return document.getElementById('dir_list');
 }
+
+function buildPath(route = -1, direction = '', stop = -1) {
+    let path = '';
+    if (route == -1)
+        return window.origin;
+    else if (direction == '')
+        return window.origin + `/routes/${route}`;
+    else if (stop == -1)
+        path = [route, direction].join('/');
+    else
+        path = [route, direction, stop].join('/');
+    return window.origin + `/routes/${path}`;
+} 
