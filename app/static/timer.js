c5327bbdbab37fe9d4ea2bf62a3162adca77d14d
Sean Rawson <seancc8@gmail.com>
2022-12-30 09:34:51 -0500

    Add functions to display timers

---
+++ app/static/timer.js
@@ -0,0 +1,26 @@
+function timer() {
+    let arrivalElems = document.getElementsByClassName("arrival_time");
+    let arrivalTimes = Array.from(arrivalElems).map(x => x.getAttribute("data-time") - 1);
+    for (i = 0; i < arrivalElems.length; i++) {
+        arrivalElems[i].innerHTML = mapToClockTime(arrivalTimes[i]);
+        arrivalElems[i].setAttribute("data-time", arrivalTimes[i]);
+    }
+
+    setTimeout(timer, 1000);
+}
+
+function mapToClockTime(time) {
+    h = Math.floor(time / 3600);
+    m = Math.floor((time - h*3600) / 60);
+    s = time - h*3600 - m*60;
+
+    let t = [];
+    if (h)
+        t = [h, m, s];
+    else if (m)
+        t = [m, s];
+    else
+        t = [s];
+
+    return t.map(x => ("00" + x).slice(-2)).join(":");
+}
