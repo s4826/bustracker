a392c4d531f3650e600f46692f857c3dcd5baf73
Sean Rawson <seancc8@gmail.com>
2023-01-02 08:09:14 -0500

    Change direction matching

--- app/app.py
+++ app/app.py
@@ -15,12 +15,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = environ['SECRET_KEY']
 
 bootstrap = Bootstrap(app)
-
+directions = {'outbound': 0, 'inbound': 1}
 
 @app.before_request
 def create_forms():
-    # avoid re-creating forms on requests for static content
-    if ("static" not in request.endpoint):
+    # don't re-create forms on requests for static content
+    if ('static' not in request.endpoint):
         if 'route_form' not in g:
             g.route_form = create_route_form()
         if 'direction_form' not in g:
@@ -29,17 +29,11 @@ def create_forms():
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/routes/<route_id>', methods = ['GET', 'POST'])
-@app.route('/routes/<route_id>/<direction>', methods = ['GET', 'POST'])
 def index(route_id=None, direction=None, stop_id=None):
     if route_id is not None and direction is not None:
 
-            dir_id = 0 if direction == "Outbound" else 1
-            route_stops = api.build_route_dict(route_id, dir_id)
-
-            return render_template('choose_stop.html',
-                                   route_form=g.route_form,
-                                   direction_form=g.direction_form,
-                                   route_stops=route_stops)
+        dir_id = directions[direction]
+        route_stops = api.build_route_dict(route_id, dir_id)
 
         return render_template('choose_dir.html', route_form=g.route_form,
                                direction_form=g.direction_form)
@@ -50,7 +44,7 @@ def index(route_id=None, direction=None, stop_id=None):
 
 @app.route('/routes/<route_id>/<direction>/<stop_id>', methods = ['GET'])
 def get_stop_predictions(route_id, direction, stop_id):
-    dir_id = 0 if direction == "Outbound" else 1
+    dir_id = directions[direction]
     predictions = sorted(api.get_arrival_times(route_id, dir_id, stop_id))
     return render_template('show_stop_times.html', route_form=g.route_form,
                            direction_form=g.direction_form,
@@ -60,14 +54,14 @@ def get_stop_predictions(route_id, direction, stop_id):
 @Cache(seconds=30)
 def create_route_form():
     route_form = RouteForm()
-    route_choices = [""] + api.get_all_route_ids()
+    route_choices = [''] + api.get_all_route_ids()
     route_form.route_list.choices = route_choices
     return route_form
 
 
 @Cache(seconds=30)
 def create_direction_form():
-    dir_choices = ["", "Outbound", "Inbound"]
+    dir_choices = [''] + list(directions.keys())
     direction_form = DirectionForm()
     direction_form.dir_list.choices = dir_choices
     return direction_form
