48de4a1cbe114b3c3230ec45057a8849de5d6214
Sean Rawson <seancc8@gmail.com>
2023-01-03 17:42:41 -0500

    Add css and change templates

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 
@@ -30,11 +30,7 @@ def create_forms():
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/routes/<route_id>', methods = ['GET', 'POST'])
 def index(route_id=None, direction=None, stop_id=None):
-    if route_id is not None and direction is not None:
-
-        dir_id = directions[direction]
-        route_stops = api.build_route_dict(route_id, dir_id)
-
+    if route_id is not None:
         return render_template('choose_dir.html', route_form=g.route_form,
                                direction_form=g.direction_form)
 
@@ -45,11 +41,11 @@ def index(route_id=None, direction=None, stop_id=None):
 @app.route('/routes/<route_id>/<direction>', methods = ['GET', 'POST'])
 def choose_stop(route_id, direction):
     dir_id = directions[direction]
-    route_stops = api.build_route_dict(route_id, dir_id)
+    session['route_stops'] = api.build_route_dict(route_id, dir_id)
     return render_template('choose_stop.html',
                            route_form=g.route_form,
                            direction_form=g.direction_form,
-                           route_stops=route_stops)
+                           route_stops=session['route_stops'])
 
 
 @app.route('/routes/<route_id>/<direction>/<stop_id>', methods = ['GET'])
@@ -58,7 +54,8 @@ def get_stop_predictions(route_id, direction, stop_id):
     predictions = sorted(api.get_arrival_times(route_id, dir_id, stop_id))
     return render_template('show_stop_times.html', route_form=g.route_form,
                            direction_form=g.direction_form,
-                           predictions=predictions)
+                           predictions=predictions,
+                           stop_name=session['route_stops'][stop_id])
 
 
 @Cache(seconds=30)
