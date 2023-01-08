4588d948e8a5520da54dacfad43903fae92bac11
Sean Rawson <seancc8@gmail.com>
2023-01-08 10:57:13 -0500

    Update db models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,33 @@
+from sqlalchemy import Table, MetaData
+from sqlalchemy.orm import relationship
+from . import db
 
-class Route(db.Model):
-    __tablename__ = "Routes"
+association_table = Table(
+    'association_table',
+    db.Model.metadata,
+    db.Column('user_id', db.ForeignKey('users.id'), primary_key=True),
+    db.Column('stop_id', db.ForeignKey('stops.id'), primary_key=True),
+)
+
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    mbta_id = db.Column(db.String(32), nullable=False, unique=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(128))
+    favorites = relationship('Stop', secondary=association_table,
+                             back_populates='user_list')
+
+    def __repr__(self):
+        return "<User '%s'>" % self.username
+
+class Stop(db.Model):
+    __tablename__ = 'stops'
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.String(32), nullable=False)
+    name = db.Column(db.String(64))
+    last_modified = db.Column(db.DateTime)
+    user_list = relationship("User", secondary=association_table,
+                             back_populates='favorites')
+
+    def __repr__(self):
+        return "<Stop '%d'>" % self.id
