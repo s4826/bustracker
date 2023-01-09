382d04d18ead06695795c867f67d0f3e10b5324e
Sean Rawson <seancc8@gmail.com>
2023-01-09 17:08:05 -0500

    Change login form

--- app/login/forms.py
+++ app/login/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, ValidationError
+from wtforms import (StringField, PasswordField, BooleanField, SubmitField,
+                    ValidationError)
 from wtforms.validators import InputRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators = [InputRequired()])
     password = PasswordField('Password', validators = [InputRequired(),
                                                        Length(8,50)])
+    remember = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
