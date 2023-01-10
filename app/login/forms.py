44cdc38b521bc7bdbce69856097aee98a0da6fee
Sean Rawson <seancc8@gmail.com>
2023-01-10 10:13:24 -0500

    Add registration form

--- app/login/forms.py
+++ app/login/forms.py
@@ -1,11 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, BooleanField, SubmitField,
                     ValidationError)
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, Email
+from email_validator import validate_email
+
+def passwords_match(form, confirm_password):
+    if form.password.data != confirm_password.data:
+        raise ValidationError('Passwords do not match')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators = [InputRequired()])
+    email = StringField('Email', validators = [InputRequired()])
     password = PasswordField('Password', validators = [InputRequired(),
                                                        Length(8,50)])
     remember = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email (Username)',
+            validators = [InputRequired(), Email()])
+    password = PasswordField('Password',
+            validators = [InputRequired(), Length(8,50)])
+    confirm_password = PasswordField('Confirm Password',
+            validators = [InputRequired(), Length(8,50), passwords_match])
+    submit = SubmitField('Register')
