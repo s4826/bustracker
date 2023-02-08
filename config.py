43652906f2904c17bc02e9c5bcbea42f722e61bc
Sean Rawson <seancc8@gmail.com>
2023-02-08 15:07:47 -0500

    Move configuration definitions to .env files

--- config.py
+++ config.py
@@ -8,28 +8,25 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     config = dotenv_values(".env")
     SECRET_KEY = config['SECRET_KEY']
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_TYPE = 'filesystem'
-    SESSION_COOKIE_SAMESITE = 'Lax'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = config['SQLALCHEMY_TRACK_MODIFICATIONS']
+    SESSION_TYPE = config['SESSION_TYPE']
+    SESSION_COOKIE_SAMESITE = config['SESSION_COOKIE_SAMESITE']
 
 class DevelopmentConfig(Config):
     config = dotenv_values(".env.dev")
-    DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(base_dir, 'db/data.sqlite')
+        os.path.join(base_dir, 'db/dev.sqlite')
     MAIL_USERNAME = config['MAIL_USERNAME']
     MAIL_PASSWORD = config['MAIL_PASSWORD'] 
     MAIL_SERVER = config['MAIL_SERVER'] 
     MAIL_PORT = config['MAIL_PORT'] 
     MAIL_FROM = config['MAIL_FROM'] 
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_USE_TLS = config['MAIL_USE_TLS']
+    MAIL_USE_SSL = config['MAIL_USE_SSL']
 
 
 class TestConfig(Config):
     config = dotenv_values(".env.test")
-    DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
         os.path.join(base_dir, 'db/test.sqlite')
     MAIL_USERNAME = config['MAIL_USERNAME']
@@ -37,8 +34,8 @@ class TestConfig(Config):
     MAIL_SERVER = config['MAIL_SERVER'] 
     MAIL_PORT = config['MAIL_PORT'] 
     MAIL_FROM = config['MAIL_FROM'] 
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = False
+    MAIL_USE_TLS = config['MAIL_USE_TLS']
+    MAIL_USE_SSL = config['MAIL_USE_SSL']
 
 
 config = {
